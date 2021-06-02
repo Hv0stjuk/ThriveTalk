d4faa673f27a39a077fcee7c3063671b004af150
Hv0stjuk <wot.hvostjuk@gmail.com>
2021-06-02 16:40:09 +0300

    feat: add media

---
+++ dist/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.testimonials__slider').slick({
+    slidesToShow:1,
+    autoplay:false,
+  });
+});
+
+function burgerMenu () {
+  const burgerBtn = document.querySelector('.burger__img');
+  burgerBtn.addEventListener('click', () => {
+    document.querySelector('.header__top').classList.toggle('show');
+    document.querySelector('.header__logo').classList.toggle('hidden');
+  });
+}
+
+burgerMenu();
+//# sourceMappingURL=script.js.map
